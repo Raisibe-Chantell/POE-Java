cd5edbb5db0cf9094d44fb5783d53f03c06cde6e
Raisibe-Chantell <ST10507551@rcconnect.edu.za>
2026-09-09 15:01:50 +0200

    Add initial Maven project and HelloWorld class
    Create initial Maven scaffold and entry point: add JAVAPOE/pom.xml (groupId com.mycompany, artifactId JAVAPOE, version 1.0-SNAPSHOT, packaging jar, maven.compiler.release=26, exec.mainClass configured) and add com.mycompany.javapoe.JAVAPOE main class that prints "Hello World!".

---
+++ JAVAPOE/src/main/java/com/mycompany/javapoe/JAVAPOE.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javapoe;
+
+/**
+ *
+ * @author game
+ */
+public class JAVAPOE {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
