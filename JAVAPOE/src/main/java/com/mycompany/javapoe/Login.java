1440eddc409ff575081ec26fbd55c5c4c7c59fad
Raisibe-Chantell <ST10507551@rcconnect.edu.za>
2026-09-17 15:18:56 +0200

    Update Login.java
    added few comment

--- JAVAPOE/src/main/java/com/mycompany/javapoe/Login.java
+++ JAVAPOE/src/main/java/com/mycompany/javapoe/Login.java
@@ -9,49 +9,51 @@ package com.mycompany.javapoe;
  * @author game
  */
 public class Login {
-    String firstName;
-    String lastName;
-    String username;
-    String password;
-    String cellNumber;
+    String fName;
+    String sName;
+    String userName;
+    String pass;
+    String cellPhoneNumber;
 
     public Login(String firstName, String lastName, String username, String password, String cellNumber) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.username = username;
-        this.password = password;
-        this.cellNumber = cellNumber;
+        fName= firstName;
+        sName = lastName;
+        userName = username;
+        pass = password;
+        cellPhoneNumber = cellNumber;
     }
 
-    public boolean checkUserName() {
-        return username.contains("_") && username.length() <= 5;
+    public boolean checkUserName(String username) {
+        return userName.contains("_") && userName.length() <= 5;
     }
 
-    public boolean checkPasswordComplexity() {
-        if (password.length() < 8) return false;
+    public boolean checkPasswordComplexity(String pass) {
+        if (pass.length() < 8) return false;
         boolean cap = false;
          boolean num = false;
          boolean spec = false;
-        for (char c : password.toCharArray()) {
+         
+        for (char c : pass.toCharArray()) {
             if (Character.isUpperCase(c)) cap = true;
             if (Character.isDigit(c)) num = true;
             if (!Character.isLetterOrDigit(c)) spec = true;
         }
         return cap && num && spec;
     }
-
-    public boolean checkCellPhoneNumber() {
-        return cellNumber.matches("^\\+27[0-9]{9}$");
+    
+    //reference : Oracle Java - Pattern class regex for validating international
+    public boolean checkCellPhoneNumber(String cellPhoneNumber) {
+        return cellPhoneNumber.matches("^\\+27[0-9]{9}$");
     }
 
-    public String registerUser() {
-        if (!checkUserName()) {
+    public String registerUser(String username) {
+        if (!checkUserName(username)) {
             return "Username is not correctly formatted, please ensure that your username contains an underscore and is no more than five characters in length.";
         }
-        if (!checkPasswordComplexity()) {
+        if (!checkPasswordComplexity(pass)) {
             return "Password is not correctly formatted, please ensure that the password contains at least eight characters, a capital letter, a number, and a special character.";
         }
-        if (!checkCellPhoneNumber()) {
+        if (!checkCellPhoneNumber(cellPhoneNumber)) {
             return "Cell phone number incorrectly formatted or does not contain international code.";
         }
         return "Username successfully captured.\nPassword successfully captured.\nCell phone number successfully added.";
@@ -59,15 +61,15 @@ public class Login {
 
     // This is the method
     public boolean loginUser(String enteredUser, String enteredPass) {
-        return enteredUser.equals(username) && enteredPass.equals(password);
+        return enteredUser.equals(userName) && enteredPass.equals(pass);
     }
 
     public String returnLoginStatus(boolean isLoggedIn) {
         if (isLoggedIn) {
-            // True message from your table
-            return "Welcome " + firstName + ", " + lastName + " it is great to see you again.";
+            // True 
+            return "Welcome " + fName + ", " + sName + " it is great to see you again.";
         } else {
-            // False message from your table
+            // False 
             return "Username or password incorrect, please try again.";
         }
     }
