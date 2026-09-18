4a089de26d6b9d959a30739cb3799fd158cd6e83
Raisibe-Chantell <ST10507551@rcconnect.edu.za>
2026-09-18 10:22:26 +0200

    Update JAVAPOE.java
    changed private to public

--- JAVAPOE/src/main/java/com/mycompany/javapoe/JAVAPOE.java
+++ JAVAPOE/src/main/java/com/mycompany/javapoe/JAVAPOE.java
@@ -12,7 +12,7 @@ import java.util.Scanner;
  */
 public class JAVAPOE {
 
-    public static void main(String[] args) { 
+    public static void main(String[] args) {
         //Declarations of virables
         String firstName;
         String username;
